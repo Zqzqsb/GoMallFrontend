f63f532c48a5277f38b23e12c3a563c9afe82dd8
ZQ <zqzqsb@gmail.com>
2024-12-03 20:31:17 +0800

    chore: resolve path && add json support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}', // 指定包含 Tailwind CSS 类名的文件
-  ],
-  theme: {
-    extend: {}, // 可自定义主题
-  },
-  plugins: [],
+	important: true, // 确保 tailwind css 有更高的优先级
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: ['Noto Sans SC', 'Graphik', 'sans-serif'],
+			serif: ['Noto Serif SC', 'Merriweather', 'serif'],
+			roboto: ['Roboto', 'sans-serif'],
+		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
+			'3xl': '1720px',
+		},
+		minHeight: {
+			60: '60px',
+		},
+	},
+	plugins: [require('tailwindcss-animated')], // 扩展动画效果
 };
