cd68c362eafffd22aed3db9dc670824c128cb01f
ZQ <zqzqsb@gmail.com>
2024-12-03 18:03:37 +0800

    feat(app): import vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './index.css'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Tailwind CSS
+import './index.css';
+
+// Vuetify 3
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({ components, directives });
+
+createApp(App).use(vuetify).mount('#app');
