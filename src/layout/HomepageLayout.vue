593cfb0a4f7824f6fdd0e2b85b72021730cb4c2f
ZQ <zqzqsb@gmail.com>
2025-02-01 00:24:01 +0800

    feat(homepage): add homepage layout

---
+++ src/layout/HomepageLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="homepage-layout">
+        <slot></slot>
+        <Footer />
+    </div>
+</template>
+
+<script setup lang="ts">
+import Footer from '@/pages/homepage/Footer.vue';
+</script>
+
+<style scoped>
+.homepage-layout {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+}
+</style>
